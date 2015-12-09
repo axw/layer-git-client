c486e487a0efa8aff827110db889dfadf697eff1
Andrew Wilkins <axwalk@gmail.com>
2015-12-09 01:29:46 -0800

    Oll korrect

--- reactive/git.py
+++ reactive/git.py
@@ -1,9 +1,10 @@
 import os
-import yaml
+import shutil
 import subprocess
+import textwrap
 
 from charmhelpers.core.hookenv import (
-    remote_service_name,
+    service_name,
     status_set,
     open_port,
     config,
@@ -11,17 +12,10 @@ from charmhelpers.core.hookenv import (
     storage_get,
 )
 
-from charmhelpers.core.templating import render
-from charmhelpers.core.host import (
-    adduser,
-    service_restart,
-    service_running,
-    service_start,
-)
+from charmhelpers.core import host
 
 from charmhelpers.fetch import (
     apt_install,
-    install_remote,
 )
 
 from charms.reactive import (
@@ -34,43 +28,71 @@ from charms.reactive import (
 )
 
 
-@when('git.available')
-@when_not('git.username.available')
-def wait_username():
-    status_set('waiting', 'Waiting for client username')
+# created in the charm directory.
+SSH_IDENTITY = 'id_rsa'
+SSH_USER_KNOWN_HOSTS_FILE = 'known_hosts'
+GIT_SSH = 'git-ssh'
+
+
+@hook('install')
+def install_git():
+    apt_install('git')
+
+
+@when('git.related')
+@when_not('git.configured')
+def configure_git(git):
+    status_set('maintenance', 'Configuring ssh and git')
+    username = service_name()
+    subprocess.check_call(['ssh-keygen', '-P', '', '-f', SSH_IDENTITY])
+    public_key = open(SSH_IDENTITY + '.pub').read()
+    git.configure(username, public_key)
+    status_set('waiting', 'Waiting for git repository to be created')
+    set_state('git.configured')
 
 
 @when('git.available')
-@when_not('git.public-key.available')
-def wait_public_key():
-    status_set('waiting', 'Waiting for client public key')
+@when_not('git.repo.cloned')
+def clone_repo(git):
+    url = git.url()
+    status_set('waiting', 'Cloning {}'.format(url))
+
+    env = os.environ.copy()
+    if git.get_remote('protocol') == 'ssh':
+        env['GIT_SSH'] = os.path.abspath(GIT_SSH)
+        hostname = git.get_remote('hostname')
+        ssh_host_key = git.get_remote('ssh-host-key')
+        write_git_ssh(hostname, ssh_host_key)
+
+    path = repo_path()
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    subprocess.check_call(['git', 'clone', url, path], env=env)
+    # TODO(axw) store the current commit in local state
+    status_set('active', '')
+    set_state('git.repo.cloned')
 
 
-@when('git.username.available', 'git.public-key-available')
-@when_not('git.repo.created')
-def create_repo(git):
-    repo_path = create_repo(repo_name,
-                            git.get_remote('username'),
-                            git.get_remote('public-key'))
-    git.configure(repo_path)
-    set_state('git.repo.created')
+# TODO(axw) when('git.commit.changed')
+# TODO(axw) need to check if hostname changed, update remote
 
 
-def create_repo(repo_name, username, public_key):
-    service = remote_service_name()
-    repo_path = os.path.join(repo_root(), service+'.git')
-    host.add_group(service)
-    host.adduser(service, password=password, shell='/usr/bin/git-shell')
-    host.add_user_to_group(service, service)
-    # TODO(axw) write public key to ~user/.ssh/authorized_keys
-    host.mkdir(repo_path, group=service, perms=0o770)
-    subprocess.check_call([
-        'git', 'init', '--bare', '--shared=group', repo_path,
-    ])
-    return repo_path
+def write_git_ssh(hostname, ssh_host_key):
+    # TODO(axw) validate format of ssh_host_key
+    content = '{} {}'.format(hostname, ssh_host_key)
+    host.write_file(SSH_USER_KNOWN_HOSTS_FILE, content, 'root', 'root', 0o600)
+
+    content = textwrap.dedent("""\
+    #!/bin/bash
+    exec /usr/bin/ssh -i {} -o UserKnownHostsFile={} $*
+    """.format(os.path.abspath(SSH_IDENTITY), os.path.abspath(SSH_USER_KNOWN_HOSTS_FILE)))
+    host.write_file(GIT_SSH, content, 'root', 'root', 0o700)
 
 
-def repo_root():
-    return storage_get('location', storage_list('repo-root')[0])
-
+def repo_path():
+    # TODO(axw) when reactive support for storage is fixed, use
+    # storage.
+    #return storage_get('location', storage_list('repo')[0])
+    path = 'repo'
+    return path
 
